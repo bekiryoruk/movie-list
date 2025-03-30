2d0eae15b66b59260b5a996b5fed0eec8f321325
bekiryoruk <bekiryoruk1@gmail.com>
2025-03-30 23:36:03 +0300

    moved base url to env file and created example env file

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
-const BASE_URL = 'https://www.omdbapi.com';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const api = axios.create({
 	baseURL: BASE_URL,
